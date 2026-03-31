8aaefded376ad7b16ea8ac371da5cf555aabadaa
Sahil Jadhav <jadhavsahil0027@gmail.com>
2026-03-31 22:58:32 +0530

    Create strings-basic.cpp

---
+++ strings-basic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str, s;
+    cin>>str;
+    cin>>s;
+    string s2 = str+s;
+    cout<<str.length()<<" "<<s.length()<<endl;
+    cout<<s2<<endl;
+    string s4, s3;
+     swap(s[0], str[0]);
+    
+    
+    cout<<str<<" "<<s;
+    
+  
+    return 0;
+}
