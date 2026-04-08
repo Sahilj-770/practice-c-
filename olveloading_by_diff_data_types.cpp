f5cf07397415c568fcdb0c3606f1bb53c750fcd5
Sahil Jadhav <jadhavsahil0027@gmail.com>
2026-04-08 16:01:15 +0530

    Create olveloading_by_diff_data_types.cpp

---
+++ olveloading_by_diff_data_types.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class base{
+    public:
+    void area(int a, int b){
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        
+        cout<<"the integers have been swapped, first one now is "<<a<<" second one now is "<<b<<endl;
+    }
+
+    void area(float c, float d){
+        float temp;
+        temp = c;
+        c = d;
+        d = temp;
+        
+        cout<<"the floats have been swapped, first one now is "<<c<<" second one now is "<<d<<endl;
+        
+    }
+
+    
+};
+
+int main(){
+    int a, b;
+    float c, d;
+    cout<<"sequence is first 2 integers, second 2 floats "<<endl;
+    cin>>a>>b>>c>>d;
+    base obj;
+    obj.area(a, b);
+    obj.area(c, d);
+    return 0;
+}
+
